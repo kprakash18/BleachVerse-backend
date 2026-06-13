21597dcd693a169db254a6afff7650924a6645f8
kprakash18 <iamkprakash18@gmail.com>
2026-06-13 17:08:02 +0530

    seed zampakuto alias

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,26 +1,26 @@
 import prisma from "../src/database/prisma.js";
-// import { seedOrganization } from "./seed/seeders/organization.seeder.js";
-// import { seedRace } from "./seed/seeders/race.seeder.js";
-// import { seedLocations } from "./seed/seeders/location.seeder.js";
-// import { seedArcs } from "./seed/seeders/arc.seeder.js";
-// import { seedEpisodes } from "./seed/seeders/episode.seeder.js";
+import { seedOrganization } from "./seed/seeders/organization.seeder.js";
+import { seedRace } from "./seed/seeders/race.seeder.js";
+import { seedLocations } from "./seed/seeders/location.seeder.js";
+import { seedArcs } from "./seed/seeders/arc.seeder.js";
+import { seedEpisodes } from "./seed/seeders/episode.seeder.js";
 import { seedCharacter } from "./seed/seeders/character.seeder.js";
-// import { seedCharacterRaces } from "./seed/seeders/characterRace.seeder.js";
-// import { seedCharacterOrganizations } from "./seed/seeders/characterOrganization.seeder.js";
-// import { seedCharacterAliases } from "./seed/seeders/characterAlias.seeder.js";
+import { seedCharacterRaces } from "./seed/seeders/characterRace.seeder.js";
+import { seedCharacterOrganizations } from "./seed/seeders/characterOrganization.seeder.js";
+import { seedCharacterAliases } from "./seed/seeders/characterAlias.seeder.js";
 import { seedZanpakuto } from "./seed/seeders/zanpakuto.seeder.js";
 import { seedZanpakutoAliases } from "./seed/seeders/zanpakutoAlias.seeder.js";
 
 async function main() {
-  // await seedOrganization();
-  // await seedRace();
-  // await seedLocations();
-  // await seedArcs();
-  // await seedEpisodes();
+  await seedOrganization();
+  await seedRace();
+  await seedLocations();
+  await seedArcs();
+  await seedEpisodes();
   await seedCharacter();
-  // await seedCharacterRaces();
-  // await seedCharacterOrganizations();
-  // await seedCharacterAliases();
+  await seedCharacterRaces();
+  await seedCharacterOrganizations();
+  await seedCharacterAliases();
   await seedZanpakuto();
   await seedZanpakutoAliases();
 }
