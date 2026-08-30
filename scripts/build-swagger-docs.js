c712c5537cb5b2979b595004358b88be6a4562db
kprakash18 <iamkprakash18@gmail.com>
2026-08-30 15:30:52 +0530

    refactor(docs): rename openapi references and test directory to swagger

--- scripts/build-swagger-docs.js
+++ scripts/build-swagger-docs.js
@@ -2,9 +2,9 @@ import { mkdir, writeFile } from "node:fs/promises";
 import swaggerSpec from "../src/docs/swagger.js";
 
 const outputDir = new URL("../public/swagger/", import.meta.url);
-const specPath = new URL("openapi.json", outputDir);
+const specPath = new URL("swagger.json", outputDir);
 
 await mkdir(outputDir, { recursive: true });
 await writeFile(specPath, `${JSON.stringify(swaggerSpec, null, 2)}\n`);
 
-console.log("Swagger docs written to public/swagger/openapi.json");
+console.log("Swagger docs written to public/swagger/swagger.json");
